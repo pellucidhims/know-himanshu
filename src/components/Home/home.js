6e0ae94ba4fc484aab146c5ca8364ae8f785d2a2
Himanshu.Himanshu@gds.ey.com <himanshu.himanshu@gds.ey.com>
2020-07-18 18:10:52 +0530

    Adding eslint, prettier, husky rules and hooks

---
+++ src/components/Home/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        border: '1px solid grey',
+        background: 'black',
+        color: 'yellow',
+      }}
+    >
+      <h1>Initial Config Setup</h1>
+    </div>
+  );
+};
+
+export default Home;
