4667c2cb2345ee825b3044af92ea5864d313a842
Himanshu.Himanshu@gds.ey.com <himanshu.himanshu@gds.ey.com>
2020-07-29 12:45:54 +0530

    Updates

---
+++ precache-manifest.c2cb9f88d24c16bccfce2ae2aff6064d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0474b109fb0bdcb09307cb6a583d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c19ce0ec54a2b4734bc",
+    "url": "/static/css/main.2f0d6e39.chunk.css"
+  },
+  {
+    "revision": "f8665933bf11015b9a6c",
+    "url": "/static/js/2.c6507f45.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.c6507f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c19ce0ec54a2b4734bc",
+    "url": "/static/js/main.45324e2c.chunk.js"
+  },
+  {
+    "revision": "c81cb1fd66eae40c437b",
+    "url": "/static/js/runtime-main.deb98ec6.js"
+  }
+]);
